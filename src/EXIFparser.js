542076a998bbd36eece5a8d1077283cae855f5ca
Jon K <Johnakao@gmail.com>
2021-04-21 13:50:16 -0700

    EXIF Features

--- src/EXIFparser.js
+++ src/EXIFparser.js
@@ -1,18 +1,11 @@
-window.onload=getExif;
-
-function getExif(imgState = {}) {
-    var img1 = document.getElementById("image");
-    EXIF.getData(img1, function() {
-        var make = EXIF.getTag(this, "Make");
-        var model = EXIF.getTag(this, "Model");
-        var makeAndModel = document.getElementById("makeAndModel");
-        makeAndModel.innerHTML = `${make} ${model}`;
-    });
-
-    var img2 = document.getElementById("image");
-    EXIF.getData(img2, function() {
-        var allMetaData = EXIF.getAllTags(this);
-        var allMetaDataSpan = document.getElementById("allMetaDataSpan");
-        allMetaDataSpan.innerHTML = JSON.stringify(allMetaData, null, "\t");
-    });
-}
+// ;
+ 
+// function getExif() {
+ 
+//     let img = document.getElementById("image");
+//     EXIF.getData(img, function() {
+//         var allMetaData = EXIF.getAllTags(this);
+//         var allMetaDataSpan = document.getElementById("allMetaDataSpan");
+//         allMetaDataSpan.innerHTML = JSON.stringify(allMetaData, null, "\t");
+//     });
+// }
--- src/GetExif.js
+++ src/GetExif.js
@@ -1,16 +1,17 @@
-window.onload=getExif;
+// window.onload=getExif;
 
-function GetExif() {
-    function getExif() {    
-        var img2 = document.getElementById("image");
-        EXIF.getData(img2, function() {
-            var allMetaData = EXIF.getAllTags(this);
-            var allMetaDataSpan = document.getElementById("allMetaDataSpan");
-            allMetaDataSpan.innerHTML = JSON.stringify(allMetaData, null, "\t");
-        });
+// function GetExif() {
+//     function getExif() {    
+//         var img2 = document.getElementById("image");
+//         EXIF.getData(img2, function() {
+//             var allMetaData = EXIF.getAllTags(this);
+//             var allMetaDataSpan = document.getElementById("allMetaDataSpan");
+//             allMetaDataSpan.innerHTML = JSON.stringify(allMetaData, null, "\t");
+//         });
+//     }
+//     return (
+//         <div></div>
+//     )
+// }
 
-    }
-
-}
-
-export default GetExif;
+// export default GetExif;
